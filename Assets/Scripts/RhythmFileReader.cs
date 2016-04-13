c5a4a78bcd22799d079faecb8e53cebfb601d830
Tim Bonin <tgbonin@gmail.com>
2016-04-13 15:39:00 -0400

    Made changes to file loading to make it compatable with android

--- Assets/Scripts/RhythmFileReader.cs
+++ Assets/Scripts/RhythmFileReader.cs
@@ -24,34 +24,44 @@ public class RhythmFileReader : MonoBehaviour {
         times = new List<float>();
         notePositions = new List<int>();
 
-        string line;
-        StreamReader fileReader = new StreamReader(songName, Encoding.Default);
-        using (fileReader)
+        TextAsset song = Resources.Load<TextAsset>("Songs/test") as TextAsset;
+        string[] lines = song.text.Split('\n');
+        for (int i = 1; i < lines.Length; i++)
         {
-            bool firstLine = true;
-            do
-            {
-                line = fileReader.ReadLine();
-                //Debug.Log("Loaded Line: " + line);
-
-                if(line != null)
-                {
-                    if (firstLine)
-                    {
-                        bpm = int.Parse(line);
-                        firstLine = false;
-                    }
-                    else
-                    {
-                        string[] note = line.Split(',');
-                        //Debug.Log(note[0] + "   " + note[1]);
-                        times.Add((float.Parse(note[0])));
-                        notePositions.Add((int.Parse(note[1])));
-                    }
-                }
-            }
-            while (line != null);
+            Debug.Log(lines[i]);
+            string[] note = lines[i].Split(',');
+            times.Add(float.Parse(note[0]));
+            notePositions.Add(int.Parse(note[1]));
         }
+
+        //string line;
+        //StreamReader fileReader = new StreamReader(songName, Encoding.Default);
+        //using (fileReader)
+        //{
+        //    bool firstLine = true;
+        //    do
+        //    {
+        //        line = fileReader.ReadLine();
+        //        //Debug.Log("Loaded Line: " + line);
+        //
+        //        if(line != null)
+        //        {
+        //            if (firstLine)
+        //            {
+        //                bpm = int.Parse(line);
+        //                firstLine = false;
+        //            }
+        //            else
+        //            {
+        //                string[] note = line.Split(',');
+        //                //Debug.Log(note[0] + "   " + note[1]);
+        //                times.Add((float.Parse(note[0])));
+        //                notePositions.Add((int.Parse(note[1])));
+        //            }
+        //        }
+        //    }
+        //    while (line != null);
+        //}
 
         GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().OnSongLoaded(times, notePositions);
     }
